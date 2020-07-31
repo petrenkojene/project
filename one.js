10643c051d961b7df1f737a84ef9979641090558
Jene <petrenkojene@gmail.com>
2020-07-31 16:07:50 +0300

    first commit

---
+++ one.js
@@ -0,0 +1,9 @@
+'use strict';
+/*console.log('ghbdtn\n' + 'dfdffdf');
+console.log(`ghbdtndfdffdf`);*/
+let number = true;
+console.log(!number);
+alert(number);
+let number = true;
+console.log(!number);
+alert(number);
